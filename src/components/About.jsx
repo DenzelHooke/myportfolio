3849717b0ad815f35d207d74de9414748458433f
denzel hooke <denzelhooke@hotmail.com>
2022-04-22 01:37:05 -0500

    Updated minor scss

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -10,6 +10,8 @@ import { BsGithub } from 'react-icons/bs'
 import { FaGitAlt } from 'react-icons/fa'
 import { SiMysql } from 'react-icons/si'
 import { SiDjango } from 'react-icons/si'
+import { SiDocker } from 'react-icons/si'
+import { FaSass } from 'react-icons/fa'
 import { gsap } from 'gsap';
 
 
@@ -22,6 +24,19 @@ function About() {
       trigger: elRef.current,
       start: 'top 25%',
     }})
+
+    const tooltips = document.querySelectorAll('.tooltip');
+
+    tooltips.forEach((item, index) => {
+      const tooltip_text = document.createElement('span');
+      tooltip_text.classList.add('tooltip-text');
+      // console.log(item.children[0].id)
+      tooltip_text.appendChild(document.createTextNode(item.children[0].id))
+      
+      item.appendChild(tooltip_text);
+    }) 
+    
+
   }, [])
 
   return (
@@ -38,27 +53,55 @@ function About() {
               <div className="item bolden">
                 <span>Frontend</span>
                 <div className="flex skill-icons">
-                  {<FaReact size={iconSize}/>}
-                  {<SiJavascript size={iconSize}/>}
-                  {<SiHtml5 size={iconSize}/>}
-                  {<SiCss3 size={iconSize}/>}
+                  <div className="tooltip">{
+                    <FaReact size={iconSize} id="React"/>}
+                  </div>
+                  <div className="tooltip">{
+                    <SiJavascript size={iconSize} id="JavaScript"/>}
+                  </div>
+                  <div className="tooltip">{
+                    <SiHtml5 size={iconSize} id="HTML5"/>}
+                  </div>
+                  <div className="tooltip">{
+                    <SiCss3 size={iconSize} id="CSS3"/>}
+                  </div>
                 </div>
               </div>
               <div className="item bolden">
                 <span>Backend</span>
                 <div className="flex skill-icons">
-                  {<SiJavascript size={iconSize}/>}
-                  {<SiPython size={iconSize}/>}
-                  {<DiNodejs size={iconSize}/>}
+                  <div className="tooltip">
+                    {<SiJavascript size={iconSize} id="JavaScript"/>}
+                  </div>
+                  <div className="tooltip">
+                    {<DiNodejs size={iconSize}  id="Node.js"/>}
+                  </div>
+                  <div className="tooltip">
+                    {<SiPython size={iconSize}  id="Python"/>}
+                  </div>
                 </div>
               </div>
               <div className="item bolden">
                 <span>Other</span>
                 <div className="flex skill-icons">
-                  {<BsGithub size={iconSize}/>}
-                  {<FaGitAlt size={iconSize}/>}
-                  {<SiMysql size={iconSize}/>}
-                  {<SiDjango size={iconSize}/>}
+                  <div className="tooltip">
+                    {<BsGithub size={iconSize} id="GitHub"/>}
+                  </div>
+                  <div className="tooltip">
+                    {<FaGitAlt size={iconSize} id="Git"/>}
+                  </div>
+                  <div className="tooltip">
+                    {<SiMysql size={iconSize}  id="MySQL"/>}
+                  </div>
+                  <div className="tooltip">
+                    {<SiDjango size={iconSize} id="Django"/>}
+                  </div>
+                  <div className="tooltip">
+                    {<SiDocker size={iconSize} id="Docker"/>}
+                  </div>
+                  <div className="tooltip">
+                    {<FaSass size={iconSize} id="Sass"/>}
+                  </div>
                 </div>
               </div>
             </div>
